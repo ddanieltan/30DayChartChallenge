ddb0b3e4a3e050325892ebc07d959a951a2d1455
Daniel Tan <danieltan.singapore@gmail.com>
2025-03-26 14:48:46 +0800

    refactor: Migrate git pushes visualization from plotnine to waffle chart

--- 2025/1-fractions.py
+++ 2025/1-fractions.py
@@ -1,69 +1,42 @@
 # %%
 import numpy as np
 import polars as pl
-from plotnine import *
+from pywaffle import Waffle
+import matplotlib.pyplot as plt
 
-
-git_pushes = pl.read_csv("../data/github/git_pushes.csv")
-git_pushes
-
-# %%
-git_pushes.describe()
-
-# %%
-df = (
-    git_pushes.group_by("iso2_code")
-    .agg(pl.sum("git_pushes"))
-    .with_columns(
-        percentage=pl.col("git_pushes") / pl.sum("git_pushes"),
-        blocks=(pl.col("git_pushes") / pl.sum("git_pushes") * 100)
-        .round()
-        .cast(pl.Int32),
-    )
+git_lang = pl.read_csv("../data/github/languages.csv")
+gl = (
+    git_lang.filter(pl.col("iso2_code") == "SG")
+    .group_by("language")
+    .agg(pl.sum("num_pushers").alias("git_pushes"))
     .sort("git_pushes", descending=True)
 )
 
-# Focus on top 5 countries and squash rest into Others
-top5 = df.head(10)
-
-others = df.slice(10).select(
-    pl.lit("Others").alias("iso2_code"), pl.sum("git_pushes").alias("git_pushes")
+n = 5
+top = gl.head(n)
+others = gl.slice(n).select(
+    pl.lit("Others").alias("language"), pl.sum("git_pushes").alias("git_pushes")
 )
 
-others = others.with_columns(
+df = pl.concat([top, others])
+df = df.with_columns(
     percentage=pl.col("git_pushes") / df["git_pushes"].sum(),
     blocks=(pl.col("git_pushes") / df["git_pushes"].sum() * 100).round().cast(pl.Int32),
 )
 
-# Combine the top 5 with the "Others" row
-result = pl.concat([top5, others])
 
-# Manual workaround rounding not equal to total of 100
-result[10, "blocks"] += 1
-result
-
-
-# %%
-x = 10
-y = 10
-
-Xlin = np.linspace(0, 5, x)
-Ylin = np.linspace(0, 5, y)
-X, Y = np.meshgrid(Xlin, Ylin)
-
-
-status = []
-for country, blocks in result.select("iso2_code", "blocks").iter_rows():
-    status += blocks * [country]
-
-df = pl.DataFrame({"x": X.flatten(), "y": Y.flatten(), "Status": status})
-
-p = ggplot(df, aes(x="x", y="y")) + geom_tile(
-    aes(fill="Status"), width=0.8, height=0.8, color="white", size=0.5
+# AI! use different shades of green as the fill color
+fig = plt.figure(
+    FigureClass=Waffle,
+    rows=5,
+    columns=20,  # Either rows or columns could be omitted
+    values=df.select("blocks").to_series(),  # Pass a list of integers to values
+    cmap_name="tab10",
 )
 
-p.show()
 
+# fig.savefig("plot.png", bbox_inches="tight")
+fig
 # %%
-df
+df.select("language").to_series().to_list()
 # %%
